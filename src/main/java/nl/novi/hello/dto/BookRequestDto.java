00fe3bbbe6af1976c0a41ecf1a3fba473ee313be
Bwiggemans <b.wiggemans@home.nl>
2021-11-26 13:29:13 +0100

    @NotBlanc varevangen door @NotEmpty

--- src/main/java/nl/novi/hello/dto/BookRequestDto.java
+++ src/main/java/nl/novi/hello/dto/BookRequestDto.java
@@ -4,20 +4,21 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 public class BookRequestDto {
 
         //Attributen
-        @NotBlank
+        @NotEmpty
         @Size(min=1, max=100)
         private String title;
 
-        @NotBlank
+        @NotEmpty
         @Size(min=1, max=100)
         private String author;
 
-        @NotBlank
+        @NotEmpty
         @Size(min=10, max=20)
         private String isbn;
 
